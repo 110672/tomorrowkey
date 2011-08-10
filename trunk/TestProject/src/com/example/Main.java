53d41950990c71b9d97b7b46a108bbed990c85d0
tomorrowkey <tomorrowkey@e63565d0-79a3-fddf-d19e-0aed18c7f374>
2011-08-10 00:41:14 +0000

    "HelloWorld!!"を"HogeHoge"に変更しました。

--- trunk/TestProject/src/com/example/Main.java
+++ trunk/TestProject/src/com/example/Main.java
@@ -3,6 +3,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("HogeHoge");
     }
 }
