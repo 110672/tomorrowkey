341f95bf5e90a1cf1a5a134a69ee5866bb490dbb
tomorrowkey <tomorrowkey@e63565d0-79a3-fddf-d19e-0aed18c7f374>
2011-08-10 00:21:15 +0000

    表示メッセージを変更しました

--- TestProject/src/com/example/Main.java
+++ TestProject/src/com/example/Main.java
@@ -3,6 +3,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello World!!");
     }
 }
